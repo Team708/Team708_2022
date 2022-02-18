15235fd88b9e99817f2339f6587dc67d91b76efe
LanceGuy5 <lanceforresth@gmail.com>
2022-02-18 16:36:25 -0500

    TurnTo and Pipeline Commands Finished

---
+++ src/main/java/frc/robot/commands/vision/SwitchPipeline.java
@@ -0,0 +1,34 @@
+package frc.robot.commands.vision;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Limelight;
+
+public class SwitchPipeline extends CommandBase{
+    
+    Limelight m_limeLight;
+
+    public SwitchPipeline(Limelight m_limeLight){
+        this.m_limeLight = m_limeLight;
+    }
+
+    @Override
+    public void initialize(){
+
+    }
+
+    @Override
+    public void execute(){
+        m_limeLight.incrementPipeline();
+    }
+
+    @Override
+    public boolean isFinished(){
+        return true;
+    }
+
+    @Override
+    public void end(boolean interrupted){
+
+    }
+
+}
