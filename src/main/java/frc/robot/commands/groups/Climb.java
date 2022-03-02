1bbdb846cea0c9dd5cf3c402b338b00075c68977
LanceGuy5 <lanceforresth@gmail.com>
2022-03-02 18:47:42 -0500

    Climb Automated Partially

--- src/main/java/frc/robot/commands/groups/Climb.java
+++ src/main/java/frc/robot/commands/groups/Climb.java
@@ -1,31 +1,35 @@
 package frc.robot.commands.groups;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.drivetrain.TurnTowardsTarget;
-import frc.robot.commands.intakeFeeder.FeederReverse;
-import frc.robot.commands.intakeFeeder.IntakeFeederOut;
-import frc.robot.commands.intakeFeeder.ShootBall;
-import frc.robot.commands.intakeFeeder.ShootFeeder;
-import frc.robot.commands.intakeFeeder.ShootIntake;
-import frc.robot.commands.intakeFeeder.StopFeeder;
-import frc.robot.commands.intakeFeeder.StopIntake;
-import frc.robot.commands.shooter.HoodUp;
-import frc.robot.commands.shooter.ShootHighGoalFar;
-import frc.robot.commands.shooter.ShootFeederStation;
-import frc.robot.commands.shooter.StopShooter;
+import frc.robot.commands.climber.ClimberArmDown;
+import frc.robot.commands.climber.ClimberArmUp;
 import frc.robot.subsystems.climber.Climber;
 import frc.robot.subsystems.drive.DriveSubsystem;
-import frc.robot.subsystems.intakeFeeder.IntakeFeeder;
-import frc.robot.subsystems.shooter.Shooter;
-import frc.robot.subsystems.vision.Limelight;
 
 public class Climb extends SequentialCommandGroup{
-    
-    public Climb(DriveSubsystem m_driveSubsystem, Climber m_Climber){
-        
-        
+
+    public Climb(DriveSubsystem m_driveSubsystem, Climber m_climber){
+        addCommands(
+            //LOOP COMPONENT OF CLIMB
+            //Bring arm down
+            new ClimberArmDown(m_driveSubsystem, m_climber),
+            new WaitCommand(2.0),
+            //HIT SWITCHES
+            //GO up distance
+            new ClimberArmUp(m_driveSubsystem, m_climber),
+            new WaitCommand(2.0),
+            //arm out
+            new RunCommand(m_climber::extendClimbingArm, m_climber),
+            new WaitCommand(2.0),
+            //release PTO
+            new RunCommand(m_climber::releasePTO, m_climber),
+            new WaitCommand(2.0),
+            //bring arm in
+            new RunCommand(m_climber::retractClimbingArm, m_climber)
+            //
+        );
     }
 
 }
