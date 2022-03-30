8c3b0745c58ae5771e6f5b9df42abab3225c2796
jpiergal <jpiergal@gmail.com>
2022-03-30 10:29:50 -0400

    added comments

--- src/main/java/frc/robot/commands/groups/Climb.java
+++ src/main/java/frc/robot/commands/groups/Climb.java
@@ -36,12 +36,12 @@ public class Climb extends SequentialCommandGroup{
 
             // to next bar
 
-            new ClimberArmUp(m_driveSubsystem, m_climber, -2000, -.3, true),  //5500, -.6     //delta off current position
+            new ClimberArmUp(m_driveSubsystem, m_climber, -2000, -.3, true),  //5500, -.6   //noswing=true,  delta off current position
             new WaitCommand(.2),        //springs do your thing                                  
             new ExtendClimbingArm(m_climber),                                           //arm out               
             new WaitCommand(1.0),       //waiting for piston
 
-            new ClimberArmUp(m_driveSubsystem, m_climber, -30000, -2, false),  //-33000, -2    //0 uses value for fully extended arm
+            new ClimberArmUp(m_driveSubsystem, m_climber, -30000, -2, false),  //-33000, -2  //noswing=false,  uses value for fully extended arm
             new WaitCommand(.2),        //springs do your thing                                  
             
 
